09817021260ccca4dc06ce653a94d85e65cdc3b5
Islombek <mustofaqulov079@gmail.com>
2024-05-07 23:13:42 +0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,12 @@ import Login from './components/Modals/Login/Login';
 import './styles/style.scss';
 
 function App() {
-  return <Login />;
+  return (
+    <>
+      <Login />
+      <h1>HEllo</h1>
+    </>
+  );
 }
 
 export default App;
